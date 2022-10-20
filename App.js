e9e047bb362c50de351a1527ff4e429034e966a2
leoniemoedl <moedl.leonie@gmail.com>
2022-10-20 23:19:20 +0200

    Add new plants specified by name only by pressing button.

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, Button, Alert, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
+  const [enteredPlantName, setEnteredPlantName] = useState('');
+  const [plants, setPlants] = useState([]);
+
+  const textInputHandler = (enteredText) => {
+    setEnteredPlantName(enteredText);
+  };
+
+  const addPlantHandler = () => {
+    setPlants(currentPlants => [...currentPlants, enteredPlantName]);
+    // Alert.alert(
+    //   "Amazing!", "You added a plant named " + enteredPlantName + ".",
+    //   [{text: "Cool"}] );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.display}>
+      <View style={styles.header}>
+        <Text>Hey Leo, here is an overview of your planties :)</Text>
+      </View>
+      {/* <StatusBar style="auto" /> */}
+      <View style={styles.plantContainer}>
+        <ScrollView>
+          {plants.map((plant) => (
+            <View key={plant} style={styles.plantItem} >
+              <Text>{plant}</Text>
+            </View>))}
+        </ScrollView>
+      </View>
+      <View style={styles.addPlantBox}>
+        <TextInput 
+          onChangeText={textInputHandler} 
+          placeholder="plant name"
+        />
+        <Button 
+          color="#30694B"
+          title="add plant" 
+          onPress={addPlantHandler} 
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  display: {
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? 40 : 0,
+    paddingRight: 20,
+    paddingBottom: 20,
+    paddingLeft: 20,
+    flex: 1
   },
+  header: {
+    flex: 1
+  },
+  plantContainer: {
+    backgroundColor: "#25523B",
+    paddingTop: 20,
+    paddingRight: 20,
+    paddingBottom: 20,
+    paddingLeft: 20,
+    flex: 8,
+  },
+  plantItem: {
+    backgroundColor: '#5AAB61',
+    padding: 10,
+    margin: 8
+  },
+  addPlantBox: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%"
+  }
 });
