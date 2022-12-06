706dac2018dbaf9290c539be35ca6a08071500fb
leoniemoedl <moedl.leonie@gmail.com>
2022-12-06 19:30:25 +0100

    display default img on reworked plantitem

--- components/PlantItem.tsx
+++ components/PlantItem.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, View, Text, Pressable } from 'react-native';
+import { StyleSheet, View, Text, Pressable, Image, ImageBackground } from 'react-native';
 import usePlant from '../features/plant/hooks/usePlant';
 import usePlantsStore from '../features/plant/hooks/usePlantsStore';
 import CustomButton from './CustomButton';
@@ -26,17 +26,20 @@ export default function PlantItem(props: PlantItemProps) {
     return (
         <Pressable>
             <View style={styles.plantItem} >
-                <Text>{plant.name}</Text>
-                <View style={styles.userInteraction} >
-                    <View style={styles.col} >
-                        <Text>Needs water in {plant.needsToBeWateredIn()} days</Text>
-                        <Text>Water rhythm: {plant.waterCycle} days</Text>
-                        <CustomButton title='give water' onPress={waterHandler} />
-                    </View>
-                    <View style={styles.col} >
-                        <Text>Neews fertilizer in {plant.needsToBeFertilazedIn()} days</Text>
-                        <Text>Fertilizer rhythm: {plant.fertilizeCycle} days</Text>
-                        <CustomButton title='fertilize' onPress={fertilizeHandler} />
+                <Image style={styles.img} source={require('../assets/plant-images/pilea.jpg')} />
+                <View style={styles.info}>
+                    <Text style={styles.title}>{plant.name}</Text>
+                    <View style={styles.container}>
+                        <View style={styles.actionContainer}> 
+                            <Text>Needs water in {plant.needsToBeWateredIn()} days</Text>
+                            <Text>Water rhythm: {plant.waterCycle} days</Text>
+                            <CustomButton title='give water' onPress={waterHandler} />
+                        </View>
+                        <View style={styles.actionContainer}> 
+                            <Text>Needs fertilizer in {plant.needsToBeFertilazedIn()} days</Text>
+                            <Text>Fertilizer rhythm: {plant.fertilizeCycle} days</Text>
+                            <CustomButton title='fertilize' onPress={fertilizeHandler} />
+                        </View>
                     </View>
                 </View>
             </View>
@@ -46,19 +49,34 @@ export default function PlantItem(props: PlantItemProps) {
 
 const styles = StyleSheet.create({
     plantItem: {
-        justifyContent: 'space-between',
-        padding: 10,
+        flexDirection: 'row',
+        height: 160,
         margin: 8,
         backgroundColor: '#2EB086',
     },
-    userInteraction: {
-        flexDirection: 'row',
-        justifyContent: 'space-between'
+    img: {
+        flex: 1,
+        width: '35%',
+        height: '100%'
     },
-    col: {
+    info: {
+        width: '65%',
+        flexDirection: 'column',
+        padding: 5
     },
-    plantBTNs: {
-        flexDirection: 'row',
+    title: {
+        fontSize: 24
     },
-
+    container: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    actionContainer: {
+        width: '50%',
+        flex: 1,
+        justifyContent: 'space-between',
+        paddingTop: 5,
+        paddingRight: 10,
+        paddingBottom: 10
+    },
 });
