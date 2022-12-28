7b4f1805c3d6002d09481c0e2c3a89884f900fe5
HaraldKangSchroeder <haraldkangschroederdevelopment@gmail.com>
2022-12-28 17:49:59 +0100

    refactor(Dtos)

--- features/plant/services/index.ts
+++ features/plant/services/index.ts
@@ -1,6 +1,3 @@
-import Service from "../../common/services/Service";
-import Plant from "../classes/Plant";
-import { CreatePlantDto } from "../dtos";
 import DumbPlantService from "./DumbPlantService";
 
 
