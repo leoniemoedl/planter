05e4029c174b8344757068aea222c62e23709465
HaraldKangSchroeder <haraldkangschroederdevelopment@gmail.com>
2023-01-07 13:54:29 +0100

    feature(IconWrapper) add icon wrapper component

---
+++ features/common/components/wrappers/Icon.tsx
@@ -0,0 +1,7 @@
+import { Icon as _Icon, IconProps } from "@rneui/themed";
+
+export default function Icon(props : IconProps) {
+    return (
+        <_Icon size={20} color='#555' {...props}/>
+    )
+}
