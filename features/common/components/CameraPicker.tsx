89da8c3b6d9309d54bb217f5d85bff446772ff1b
leoniemoedl <moedl.leonie@gmail.com>
2023-01-07 14:24:57 +0100

    refactor(CameraPicker): camera functionality as separate component

--- features/common/components/CameraPicker.tsx
+++ features/common/components/CameraPicker.tsx
@@ -1,15 +1,71 @@
-import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Alert, Image } from "react-native";
 import { Camera, CameraType } from 'expo-camera';
 import { useEffect, useState } from "react";
 import CustomButton from "../../plant/components/CustomButton";
 import { Icon } from "@rneui/themed";
 
 interface CameraPickerProps {
+    onTakePhoto: (img: string) => void;
+}
+
+export default function CameraPicker(props: CameraPickerProps) {
+    const [cameraActive, setCameraActive] = useState(false);
+    const [imageBase64, setImageBase64] = useState('');
+
+    useEffect(() => {
+        console.log(imageBase64, '- Has changed')
+    },[props.onTakePhoto(imageBase64)]); // <-- here put the parameter to listen
+
+    const takePhoto = () => {
+        setCameraActive(true);
+    }
+
+    return (
+        <View>
+            {(imageBase64 === '') ?
+                (!cameraActive) ?
+                    <View style={{ ...styles.image, backgroundColor: '#EEE6CE' }} >
+                        <CustomButton title='take image' onPress={takePhoto} />
+                    </View>
+
+                    :
+                    <View style={styles.image}>
+                        <Cam
+                            onTakePhoto={setImageBase64}
+                            onClose={() => { setCameraActive(false); }} />
+                    </View>
+                :
+                (!cameraActive) ?
+                    <View>
+                        <Image style={styles.image} source={{ uri: 'data:image/jpg;base64,' + imageBase64 }} />
+                        <TouchableOpacity onPress={takePhoto} style={styles.btn2}>
+                            <Icon
+                                name={'camera'}
+                                type='font-awesome'
+                                color={'#ffffff'}
+                                size={20}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                    :
+                    <View style={styles.image}>
+                        <Cam
+                            onTakePhoto={setImageBase64}
+                            onClose={() => { setCameraActive(false); }} />
+                    </View>
+
+            }
+        </View>
+    );
+}
+
+
+interface CamProps {
     onTakePhoto: (imgBase64: string) => void;
     onClose: () => void;
 }
 
-export default function CamerPicker(props: CameraPickerProps) {
+function Cam(props: CamProps) {
     const [hasPermission, setHasPermission] = useState(false);
     const [cameraRef, setCameraRef] = useState<null | Camera>(null)
     const [type, setType] = useState(CameraType.back);
@@ -56,7 +112,7 @@ export default function CamerPicker(props: CameraPickerProps) {
                         type='font-awesome'
                         color={'#ffffff'}
                         size={20}
-                        />
+                    />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={takePhoto} style={styles.btn}>
                     <Icon
@@ -64,7 +120,7 @@ export default function CamerPicker(props: CameraPickerProps) {
                         type='font-awesome'
                         color={'#ffffff'}
                         size={20}
-                        />
+                    />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={flipCamera} style={styles.btn}>
                     <Icon
@@ -72,7 +128,7 @@ export default function CamerPicker(props: CameraPickerProps) {
                         type='font-awesome'
                         color={'#ffffff'}
                         size={20}
-                        />
+                    />
                 </TouchableOpacity>
             </View>
         </Camera>
@@ -94,5 +150,20 @@ const styles = StyleSheet.create({
         backgroundColor: '#B8405E',
         padding: 15,
         borderRadius: 50
-    }
+    },
+    image: {
+        width: '100%',
+        height: 300,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    btn2: {
+        backgroundColor: '#cccccc',
+        opacity: 0.5,
+        padding: 15,
+        borderRadius: 50,
+        position: 'absolute',
+        alignSelf: 'center',
+        top: '40%' // TODO: calculate true vertical center
+    },
 });
